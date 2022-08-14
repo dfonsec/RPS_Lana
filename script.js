8128f0bfd427fca818b7dd278e0ff15bb43de5f0
Dan Fonseca <dfonseca@hmc.edu>
2022-08-14 18:31:59 -0400

    Added  necessary function names

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+function getComputerChoice(){
+    // code here
+}
+
+function playRound(playerSelection, computerSelection){
+    // code here
+}
+
+function game(){
+    // code here
+}
+    
+
